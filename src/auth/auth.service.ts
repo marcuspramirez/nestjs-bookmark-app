3198c4545f5acd603ccc1d8b65bedda096e0b66c
marcuspramirez <@macusramirez@gmail.com>
2022-10-19 13:26:01 -0500

    return selected fields

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,6 +16,11 @@ export class AuthService {
         email: dto.email,
         hash,
       },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
     });
     // return the saved user
     return user;
