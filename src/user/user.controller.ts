754370b0a596f2b79ca912aec9c041ebdfb322ba
marcuspramirez <@macusramirez@gmail.com>
2022-11-15 10:28:41 -0600

    JwtGuard class created and exported to user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtGuard } from '../auth/guard';
 
 @Controller('users')
 export class UserController {
   // users/me
+  @UseGuards(JwtGuard)
   @Get('me')
-  getMe() {
-    return 'user info';
+  getMe(@Req() req: Request) {
+    return req.user;
   }
 }
